3782a595d66558f14ac607f19ccb3b28aed73bcd
Rishil Kathiriya <rishil.the.analyst@gmail.com>
2025-07-22 18:02:12 +0530

    Update Labour.java

--- app/src/main/java/com/project/agriculturemanagmentapp/Labour.java
+++ app/src/main/java/com/project/agriculturemanagmentapp/Labour.java
@@ -1,7 +1,6 @@
 package com.project.agriculturemanagmentapp;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -58,6 +57,7 @@ public class Labour extends Fragment {
         }
     }
 
+    
     @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
